df35147e19e4106c3f1cb84f1b4c6ed34e7e0e4d
Diego <drbv27@gmail.com>
2022-01-12 10:50:25 -0500

    Image component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Link from "next/link";
+import Image from "next/image";
+import Coffee from "../public/coffee.jpg";
+
+//<Image src="/coffee.jpg" width={600} height="400" />
 export default function Home() {
   return (
     <div>
       <Link href="/chanchitos">Ir a chanchitos</Link>
       <p>Chanchito Feliz</p>
+      <Image src={Coffee} width={600} height="400" />
     </div>
   );
 }
