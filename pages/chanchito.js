07b850a714f38cadec2959857663eabad2e419fc
Diego <drbv27@gmail.com>
2022-01-12 07:37:10 -0500

    links and code splitting

---
+++ pages/chanchito.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Chanchito = () => {
+  return (
+    <div>
+      <p>Chanchito Triste</p>
+      <Link href="/">Inicio</Link>
+    </div>
+  );
+};
+export default Chanchito;
